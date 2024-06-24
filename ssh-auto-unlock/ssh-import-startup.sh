57a42402848ddd4a810a315f81e875c0f6d9b6f8
Dimitrios Charalampidis <dimitrios@charalampidis.pro>
2024-06-24 02:52:09 +0200

    Add id_ prefix to SSH keys
    - Key that reside in ~/.ssh must have a prefix "id_"
    - This is needed to avoid loading files such as known_hosts

--- ssh-auto-unlock/ssh-import-startup.sh
+++ ssh-auto-unlock/ssh-import-startup.sh
@@ -4,22 +4,19 @@
 kwallet-query -l kdewallet > /dev/null
 
 # Import SSH keys from the user's home directory
-for KEY in $(ls $HOME/.ssh/* | grep -v \.pub); do
+for KEY in $(ls $HOME/.ssh/id_* | grep -v \.pub); do
   ssh-add -q ${KEY} </dev/null
 done
 
 # Define a custom list of SSH keys
 SSH_KEYS_LIST=(
-	"/mnt/devel/dimitrios/ansible/ansible_ssh_key"
+	"/mnt/devel/dimitrios/deploywell/ansible/ansible_ssh_key"
 	# Add more keys here
 )
 
 # Import SSH keys from the custom list
 for KEY in ${SSH_KEYS_LIST[@]}; do
-  if [ ! -f ${KEY} ]; then
-	continue
-  fi
-  else
+  if [ -f ${KEY} ]; then
     ssh-add -q ${KEY} </dev/null
   fi
 done
